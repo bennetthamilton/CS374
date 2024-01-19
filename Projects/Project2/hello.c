e372021475a132aeafc1792bffdf1b692fc1075b
bennetthamilton <bennetthamilton0@gmail.com>
2024-01-19 13:24:26 -0800

    step 1 complete

---
+++ Projects/Project2/hello.c
@@ -0,0 +1,17 @@
+// Step #1 Shake off the dust
+
+#include <stdio.h>
+
+// define varaibles
+int x = 12;
+float y = 3.14;
+char* s = "Hello World";
+
+// print variable values
+printf("x is %d, y is %.6f\n", x, y);
+printf("%s\n", s);
+
+// print range 1 to 5 times 5
+for (int i = 0; i < 5; ++i) {
+    printf("%d x 5 = %d\n", i, i * 5);
+}
