d3917cda3a9b4b6ac6cab4e5b71de5dbfaeaf643
bennetthamilton <bennetthamilton0@gmail.com>
2024-01-19 13:31:26 -0800

    included program in main function

--- Projects/Project2/hello.c
+++ Projects/Project2/hello.c
@@ -2,16 +2,20 @@
 
 #include <stdio.h>
 
-// define varaibles
-int x = 12;
-float y = 3.14;
-char* s = "Hello World";
+int main() {
+    // define varaibles
+    int x = 12;
+    float y = 3.14;
+    char* s = "Hello World";
 
-// print variable values
-printf("x is %d, y is %.6f\n", x, y);
-printf("%s\n", s);
+    // print variable values
+    printf("x is %d, y is %.6f\n", x, y);
+    printf("%s\n", s);
 
-// print range 1 to 5 times 5
-for (int i = 0; i < 5; ++i) {
-    printf("%d x 5 = %d\n", i, i * 5);
+    // print range 1 to 5 times 5
+    for (int i = 0; i < 5; ++i) {
+        printf("%d x 5 = %d\n", i, i * 5);
+    }
+
+    return 0;
 }
