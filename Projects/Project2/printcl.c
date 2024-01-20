7da2782685ad27c2ac3556df6999d08591b97dd2
bennetthamilton <bennetthamilton0@gmail.com>
2024-01-20 11:33:31 -0800

    fixed bug in makefile, added functionality to printcl

--- Projects/Project2/printcl.c
+++ Projects/Project2/printcl.c
@@ -1,2 +1,18 @@
 // step #3 access command line arguments
 
+#include <stdio.h>
+
+// Write a program called printcl.c that prints out all the command line arguments passed to it like so:
+
+// % ./printcl aaa bb c
+// 0: ./printcl
+// 1: aaa
+// 2: bb
+// 3: c
+int main(int argc, char *argv[]) {
+    for (int i = 0; i < argc; ++i) {
+        printf("%d: %s\n", i, argv[i]);
+    }
+
+    return 0;
+}
