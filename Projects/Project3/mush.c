e9e0072685c3a872273865520331a4ae634c9cd5
bennetthamilton <bennetthamilton0@gmail.com>
2024-01-27 11:52:40 -0800

    added arg constant, put command running its own its own command, added handling for cd

--- Projects/Project3/mush.c
+++ Projects/Project3/mush.c
@@ -4,7 +4,8 @@
 #include <string.h>
 #include <sys/wait.h>
 
-#define MAX_INPUT_SIZE 1024
+#define MAX_INPUT_SIZE 2048
+#define MAX_ARGUMENTS  128
 
 void parseInput(char* input, char** args) {
 
@@ -20,9 +21,28 @@ void parseInput(char* input, char** args) {
     args[i] = NULL; // Null-terminate the array of arguments
 }
 
+void runCommand (char** args) {
+    // Fork a child process
+    pid_t pid = fork();
+
+    if (pid < 0) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    } else if (pid == 0) {
+        // Child process: execute the command
+        execvp(args[0], args);
+        // If execvp returns, an error occurred
+        perror("execvp");
+        exit(EXIT_FAILURE);
+    } else {
+        // Parent process: wait for the child to finish
+        wait(NULL);
+    }
+}
+
 int main() {
     char input[MAX_INPUT_SIZE];
-    char* args[MAX_INPUT_SIZE / 2 + 1]; // Maximum number of arguments
+    char* args[MAX_ARGUMENTS]; 
 
     while (1) {
         // Print the shell prompt
@@ -41,16 +61,20 @@ int main() {
         // Parse the input into individual words
         parseInput(input, args);
 
-        // For now, simply print the parsed arguments
-        for (int i = 0; args[i] != NULL; ++i) {
-            printf("Prompt %d: %s\n", i, args[i]);
+        // if command is cd, call system call chdir
+        if (strcmp(args[0], "cd") == 0) {
+            if (chdir(args[1]) != 0) {
+                perror("chdir");
+            }
+            continue;
         }
 
-        // TODO: Add the logic for executing commands (fork, execvp, wait)
-        // ...
+        if (args[0] == NULL) {
+            continue; // Empty command, go to the next iteration
+        }
 
-        // TODO: Add error handling and exit conditions
-        // ...
+       runCommand(args);
+
     }
 
 
