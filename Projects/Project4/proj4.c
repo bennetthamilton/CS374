9f07d9422bf9e59b75812291fd8de7bc10a0f71d
bennetthamilton <bennetthamilton0@gmail.com>
2024-02-03 15:42:04 -0800

    added functionality to both processes, now parses command line args

--- Projects/Project4/proj4.c
+++ Projects/Project4/proj4.c
@@ -13,6 +13,9 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "Usage: %s <directory>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
+
+    // parse the command line arguments
+    const char *directory = (argc > 1) ? argv[1] : ".";
     
     // create a pipe
     int pfd[2];
@@ -29,16 +32,21 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    // child process (run wc)
-    if (child_pid == 0) {
+    if (child_pid == 0) {   // child process (run wc)
         // duplicate read end of pipe to stdin
+        dup2(pfd[0], STDIN_FILENO);
         // close write end of pipe
+        close(pfd[1]);
         // run wc
-    } else {
-        // parent process (run ls)
+        execlp("wc", "wc", "-l", NULL);
+    
+    } else {    // parent process (run ls)
         // duplicate write end of pipe to stdout
+        dup2(pfd[1], STDOUT_FILENO);
         // close read end of pipe
+        close(pfd[0]);
         // run ls
+        execlp("ls", "ls", "-1a", directory, NULL);
     }
 
     return 0;
