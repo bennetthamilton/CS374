ce4d5daf877c34b90c5fb1cf80f35d1ace481878
bennetthamilton <bennetthamilton0@gmail.com>
2024-02-03 15:52:42 -0800

    added new file, set game plan

---
+++ Projects/Project4/redirout.c
@@ -0,0 +1,24 @@
+// Name: Bennett Hamilton
+// Date: 2/3/24
+// Description: This program will run a child process/program, and save that child's output in a specified file.
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main(int argc, char *argv[]) {
+    // argument check
+    
+
+    // open the output file
+
+
+    // duplicate the file descriptor to stdout
+
+
+    // close the original file descriptor
+
+
+    // run the command
+
+}
