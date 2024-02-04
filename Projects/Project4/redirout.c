70f063a933af05ed79478da2395782b13e59fc83
bennetthamilton <bennetthamilton0@gmail.com>
2024-02-03 16:02:47 -0800

    added arg check, and open, duplicate, and run (along with some error checking)

--- Projects/Project4/redirout.c
+++ Projects/Project4/redirout.c
@@ -8,17 +8,29 @@
 
 int main(int argc, char *argv[]) {
     // argument check
-    
+    if (argc < 3) {
+        perror("Usage: redirout <output file> <command> [args]");
+        return 1;
+    }
 
     // open the output file
+    int fd = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC);
 
+    if (fd == -1) {
+        perror("open");
+        return 1;
+    }
 
     // duplicate the file descriptor to stdout
-
+    dup2(fd, STDOUT_FILENO);
 
     // close the original file descriptor
-
+    close(fd);
 
     // run the command
+    execvp(argv[2], &argv[2]);
+    perror("execvp");
+
+    return 0;
 
 }
