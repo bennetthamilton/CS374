9a64aac00b2285cf84475d1c0a53e74bde0353ce
bennetthamilton <bennetthamilton0@gmail.com>
2024-02-09 13:12:07 -0800

    added files, set up methods

---
+++ Projects/Project5/llist.c
@@ -0,0 +1,43 @@
+// Name: Bennett Hamilton
+// Date: 2/9/24
+// Description: implementation of linked list 
+
+#include <llist.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void llist_insert_head(struct llist **list, int node) {
+
+}
+
+void llist_delete_head(struct llist **list) {
+
+}
+
+void llist_insert_tail(struct llist **list, int node) {
+
+}
+
+void llist_print(struct llist *list) {
+
+}
+
+void llist_free(struct llist *list) {
+
+}
+
+void node_alloc() {
+
+}
+
+void node_free(int *node) {
+
+}
+
+
+int main() {
+
+    // parse command line arguments
+
+    return 0;
+}
