13e6839b1f9c4a2d2cf73676adfe2d794b3c1c8a
bennetthamilton <bennetthamilton0@gmail.com>
2024-02-10 14:21:28 -0800

    implemented functions

--- Projects/Project5/llist.c
+++ Projects/Project5/llist.c
@@ -8,27 +8,63 @@
 
 // insert node at head, previously allocated
 void llist_insert_head(struct node **head, struct node *n) {
+    if (n == NULL) {
+        return;
+    }
 
+    n->next = *head;
+    *head = n;
 }
 
 // delete node at head, return pointer to node or NULL if list is empty
 struct node *llist_delete_head(struct node **head) {
-    return NULL;  
+    if (*head = NULL) {
+        return NULL;  
+    }
+
+    struct node *deleted = *head;   // save pointer to head
+    *head = (*head)->next;          // move head to next node
+    deleted->next = NULL;           // disconnect deleted node
+    return deleted;
 }
 
 // insert node at tail, previously allocated
 void llist_insert_tail(struct node **head, struct node *n) {
+    if (n == NULL) {
+        return;
+    }
 
+    struct node *current = *head;
+    while (current->next != NULL) { // find last node
+        current = current->next;
+    }
+    current->next = n;
 }
 
 // print list to standard output in the format: "1 -> 2 -> 3" or "[empty]"
 void llist_print(struct node *head) {
+    if (head == NULL) {
+        printf("[empty]\n");
+        return;
+    }
 
+    // loop through list and print each value
+    while (head != NULL) {
+        printf("%d", head->value);
+        if (head->next != NULL) {
+            printf(" -> ");
+        }
+        head = head->next;
+    }
+    printf("\n");
 }
 
 // free entire list, head set to NULL
 void llist_free(struct node **head) {
-
+    while (*head != NULL) {
+        struct node *deleted = llist_delete_head(head);
+        node_free(deleted);
+    }
 }
 
 // allocate new node with specified value and next pointer set to NULL
