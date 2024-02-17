c8a368ce49c557ac62aed12848c0bd3fddffb90a
bennetthamilton <bennetthamilton0@gmail.com>
2024-02-17 11:34:06 -0800

    project 6 init commit

---
+++ Projects/Project6/mymemory.c
@@ -0,0 +1,13 @@
+// Name: Bennett Hamilton
+// Date: 2/17/24
+// Description: my implementation of memory allocator
+
+
+// function to allocate memory
+void *my_malloc(int size) {
+    // initialize memory if not already
+}
+
+int main() {
+    return 0;
+}
