74ce60cc1376c8b091320089de36a30908e349a1
bennetthamilton <bennetthamilton0@gmail.com>
2024-02-21 10:45:37 -0800

    challenge files added

--- exercises/chall3.c
+++ exercises/chall3.c
@@ -1,56 +1,56 @@
-#include <stdio.h>
-#include <stdlib.h>
+// #include <stdio.h>
+// #include <stdlib.h>
 
-// Make a deep copy of a ragged array of strings
-char **alloc_copy(char **source, int count)
-{
-    char **copy = malloc(count * sizeof(char *));
+// // Make a deep copy of a ragged array of strings
+// char **alloc_copy(char **source, int count)
+// {
+//     char **copy = malloc(count * sizeof(char *));
 
-    for (int i = 0; i < count; i++) {
-        copy[i] = malloc(strlen(source[i]) + 1);
-        strcpy(copy[i], source[i]);
-    }
+//     for (int i = 0; i < count; i++) {
+//         copy[i] = malloc(strlen(source[i]) + 1);
+//         strcpy(copy[i], source[i]);
+//     }
 
-    return copy;
-}
+//     return copy;
+// }
 
-void free_copy(char **copy, int count)
-{
-    free(copy - count); 
-}
+// void free_copy(char **copy, int count)
+// {
+//     free(copy - count); 
+// }
 
-void print_ragged_array(char **array, int count)
-{
-    for (int i = 0; i < count; i++) {
-        printf("\t%s\n", array[i]);
-    }
-}
+// void print_ragged_array(char **array, int count)
+// {
+//     for (int i = 0; i < count; i++) {
+//         printf("\t%s\n", array[i]);
+//     }
+// }
 
-void hline()
-{
-    puts("----------------------------------------------");
-}
+// void hline()
+// {
+//     puts("----------------------------------------------");
+// }
 
-int main(void)
-{
-    char *source[] = {
-        "Hello!",
-        "This an array with elements of different lengths",
-        "And we wish to copy it."
-    };
+// int main(void)
+// {
+//     char *source[] = {
+//         "Hello!",
+//         "This an array with elements of different lengths",
+//         "And we wish to copy it."
+//     };
 
-    printf("Original array:\n");
-    print_ragged_array(source, 3);
-    hline();
+//     printf("Original array:\n");
+//     print_ragged_array(source, 3);
+//     hline();
 
-    char **copy = alloc_copy(source, 3);
+//     char **copy = alloc_copy(source, 3);
 
-    printf("Copy of array:\n");
-    print_ragged_array(copy, 3);
-    hline();
+//     printf("Copy of array:\n");
+//     print_ragged_array(copy, 3);
+//     hline();
 
-    // If you only make a shallow copy, this line will crash:
-    copy[0][0] = 'x';
+//     // If you only make a shallow copy, this line will crash:
+//     copy[0][0] = 'x';
 
-    free_copy(copy, 3);
-}
+//     free_copy(copy, 3);
+// }
