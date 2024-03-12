098cc03664d7210027d22bf85966ffe15e8528f8
bennetthamilton <bennetthamilton0@gmail.com>
2024-03-12 12:55:30 -0700

    read write exercise complete

--- exercises/rdwr.c
+++ exercises/rdwr.c
@@ -41,29 +41,43 @@ int main(void)
 
     create_file();
 
-    // TODO
-    // 
     // Open the FILENAME for O_RDWR int variable `fd`
+    fd = open(FILENAME, O_RDWR);
+
     // Map it with mmap:
-    //
-    //  data = mmap(NULL, data_len, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
-    //
+    data = mmap(NULL, data_len, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+    if (data == MAP_FAILED) {
+        perror("mmap");
+        exit(1);
+    }
+
     // Close the file
+    close(fd);
+
     // Fork a child process:
-    //    Child calls increment_data_a_lot()
-    //    Then munmaps the file:
-    //        munmap(data, data_len);
-    //    Then exits
-    //
+    if (fork() == 0) {
+        // Child calls increment_data_a_lot()
+        increment_data_a_lot(data);
+        // Then munmaps the file:
+        munmap(data, data_len);
+        // Then exits
+        exit(0);
+    }
+
     // Parent calls increment_data_a_lot()
+    increment_data_a_lot(data);
     // Then waits for the child to complete
+    wait(NULL);
     // Then prints the value in *data
+    printf("%lld\n", *data);
     // Then munmaps the file
+    munmap(data, data_len);
     // Then exits
-    //
+    exit(0);
+
     //  Both the parent and the child increment the value in the file
     //  100000 times. What should the result be? What is the result?
     //  If it's not 100000*2, why isn't it? How could we fix it?
-    (void)data_len;(void)fd;
+    
 }
 
